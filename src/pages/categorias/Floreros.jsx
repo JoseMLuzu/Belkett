f2db658ee994f4c83264846f97372a7fcefcbbaa
JoseMLuzu <josemanuelluzuriaga@gmail.com>
2024-12-09 22:25:43 -0500

    a deleted

--- src/pages/categorias/Floreros.jsx
+++ src/pages/categorias/Floreros.jsx
@@ -58,13 +58,12 @@ const Floreros = () => {
               key={product.id}
               className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
             >
-              <a href="#" className="zoom-container">
-                <img
-                  className="p-8 rounded-t-lg"
-                  src={product.image}
-                  alt={product.name}
-                />
-              </a>
+              <img
+                className="p-8 rounded-t-lg"
+                src={product.image}
+                alt={product.name}
+              />
+
               <div className="px-5 pb-5">
                 <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                   {product.name}
