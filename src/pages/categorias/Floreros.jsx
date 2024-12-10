404f0b30b6674ea4c9364f514a7ef09b70d2699b
JoseMLuzu <josemanuelluzuriaga@gmail.com>
2024-12-09 22:28:30 -0500

    a fix

--- src/pages/categorias/Floreros.jsx
+++ src/pages/categorias/Floreros.jsx
@@ -58,12 +58,13 @@ const Floreros = () => {
               key={product.id}
               className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
             >
-              <img
-                className="p-8 rounded-t-lg"
-                src={product.image}
-                alt={product.name}
-              />
-
+              <a className="zoom-container">
+                <img
+                  className="p-8 rounded-t-lg"
+                  src={product.image}
+                  alt={product.name}
+                />
+              </a>
               <div className="px-5 pb-5">
                 <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                   {product.name}
