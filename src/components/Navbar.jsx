9a90fc7917356c3d32cf92e38d500a45dced4c79
JoseMLuzu <josemanuelluzuriaga@gmail.com>
2024-12-10 11:22:35 -0500

    Spell checker

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -76,7 +76,7 @@ function Navbar() {
                 id="dropdownNavbarLink"
                 className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 -blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
               >
-                Catalogo
+                Catálogo
                 <svg
                   className="w-2.5 h-2.5 ms-2.5"
                   aria-hidden="true"
